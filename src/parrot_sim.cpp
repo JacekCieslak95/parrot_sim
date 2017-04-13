693241fe633c0d8c19561adf09e173b564365eda
JacekCieslak95 <jacek.cieslak95@gmail.com>
2017-04-14 01:22:59 +0200

    zmiana nazwy głównego wykonywalnego pliku z "main.cpp" do "parrot_sim.cpp"

--- src/parrot_sim.cpp
+++ src/parrot_sim.cpp
@@ -4,12 +4,17 @@
 #include "tf/transform_listener.h"
 #include <sstream>
 #include <math.h>
+#include <iostream>
 
+using namespace std;
 
 int main(int argc, char **argv)
 {
 
 	ros::init(argc, argv, "parrot_sim");
 	ros::NodeHandle n;
+
+	cout << "start" <<endl;
+
 	return 0;
 }
